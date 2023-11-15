44d30958df7b78d9904516b98b592b50a38c9f6a
David-Mateus <david-mateus09@hotmail.com>
2023-11-15 14:41:08 -0300

    User entity and resource

---
+++ src/main/java/com/davidmateus/webservice/resources/UserResource.java
@@ -0,0 +1,17 @@
+package com.davidmateus.webservice.resources;
+
+import com.davidmateus.webservice.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
+        return ResponseEntity.ok().body(u);
+    }
+}
