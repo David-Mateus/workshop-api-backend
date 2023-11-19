3e19a6b2c179ae02bfa02cd9258014fc69ac3bf3
David-Mateus <david-mateus09@hotmail.com>
2023-11-19 13:54:46 -0300

    criando enum do orderStatus

--- src/main/java/com/davidmateus/webservice/config/TestConfig.java
+++ src/main/java/com/davidmateus/webservice/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.davidmateus.webservice.config;
 
 import com.davidmateus.webservice.entities.Order;
 import com.davidmateus.webservice.entities.User;
+import com.davidmateus.webservice.entities.enums.OrderStatus;
 import com.davidmateus.webservice.repositories.OrderRepository;
 import com.davidmateus.webservice.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,9 +26,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1, OrderStatus.PAID);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2, OrderStatus.WAITING_PAYMENT);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1, OrderStatus.WAITING_PAYMENT);
         // Salvando no Banco de dados
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
